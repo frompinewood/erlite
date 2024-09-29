d4c7761b775b1217f6f80c888e865787e80101f8
Micaiah Parker <frompinewood@icloud.com>
2024-09-28 20:20:02 -0400

    new comments

--- src/erlite.hrl
+++ src/erlite.hrl
@@ -1,4 +1,6 @@
+%% @doc https://sqlite.org/rescode.html
 -define(SQLITE_OK, 0).
 -define(SQLITE_ROW, 100).
 -define(SQLITE_DONE, 101).
+%% @todo add additional resutl and error codes for future use
 -define(ERLITE_VERSION, {0, 1, 0}).
